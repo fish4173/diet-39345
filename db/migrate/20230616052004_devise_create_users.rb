ad7ed1369332df89a4e45278c5eaf993c390a12e
fish4173 <uoji4173@gmail.com>
2023-06-17 11:21:17 +0900

    ユーザ管理機能

--- db/migrate/20230616052004_devise_create_users.rb
+++ db/migrate/20230616052004_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :nickname,             null: false
       t.string :email,                null: false
-      t.string :password,             null: false
+      t.string :encrypted_password,   null: false
       t.integer :height,              null: false
       t.integer :weight,              null: false
       t.integer :age,                 null: false
