e90d55c342d84ae8c4a3a653645ffe81fb1f91aa
fish4173 <uoji4173@gmail.com>
2023-06-23 19:38:35 +0900

    DBリセット

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,8 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 
-# bundle exec rake db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rake db:migrate
+
+# デプロイするたびにDBをリセットする場合は下記の有効可、上記の文章をコメントアウトに変更
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
 
